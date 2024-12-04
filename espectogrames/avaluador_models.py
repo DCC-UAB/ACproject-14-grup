9cb60634c4fb5f1fc88e70940a7cd49690939273
carlotafp26 <1666263@uab.cat>
2024-12-04 11:14:47 +0100

    funcio logistic regression + sdg

--- espectogrames/avaluador_models.py
+++ espectogrames/avaluador_models.py
@@ -1,6 +1,6 @@
 """
 MODELS a implementar:
-Naive Bayes (BernoulliNB, CategoricalNB, GaussianNB, MultinomialNB), LogisticRegression, KNN, SGD, DecisionTree, KMeans, SVM, 
+KNN, DecisionTree, KMeans, SVM, 
 RandomForest, GradientBoosting, XGBoost (XGB, XGBRF)
 """
 
@@ -9,11 +9,18 @@ base_dir = "ACproject-14-grup/datasets/Data1/images_original"
 X_train, X_test, y_train, y_test, label_encoder = preprocess_images(base_dir)
 
 # Esquema de les crides a funcions de models (NO es codi final!!)
+
 from sklearn.naive_bayes import BernoulliNB, CategoricalNB, GaussianNB, MultinomialNB
 # Naive Bayes
 naive_bernoulli = BernoulliNB()
 naive_categorical = CategoricalNB()
 naive_gaussian = GaussianNB()
 naive_multinomial = MultinomialNB()
+
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+# Logistic Regression i SDG
+logistic_regression = LogisticRegression(max_iter=500, random_state=42)
+sdg = SGDClassifier(random_state=42)
+
 
 
