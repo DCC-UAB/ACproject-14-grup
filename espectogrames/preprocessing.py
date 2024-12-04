ebb174b957e7dd37411512dffb538758cca73ddd
carlotafp26 <1666263@uab.cat>
2024-12-04 10:24:20 +0100

    afegim opció imatges color o escala grisos

--- espectogrames/preprocessing.py
+++ espectogrames/preprocessing.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from collections import Counter
 
 # Carrega i preprocessa imatges des del directori base.
-def load_images(base_dir, img_size=(128, 128)):
+def load_images(base_dir, img_size=(128, 128),  color=False):
     if not os.path.exists(base_dir) or not os.listdir(base_dir):
         raise FileNotFoundError(f"El directori {base_dir} és buit o no existeix.")
 
@@ -16,10 +16,14 @@ def load_images(base_dir, img_size=(128, 128)):
         if os.path.isdir(genre_path):
             for img_file in os.listdir(genre_path):
                 img_path = os.path.join(genre_path, img_file)
-                img = cv2.imread(img_path, cv2.IMREAD_COLOR) # possible simplificació probar després: cv2.IMREAD_GRAYSCALE
+                if color:
+                    flag = cv2.IMREAD_COLOR  
+                else:
+                    flag = cv2.IMREAD_GRAYSCALE
+                img = cv2.imread(img_path, flag)
                 if img is not None:
-                    img_resized = cv2.resize(img, img_size) # normalització de les imatges
-                    data.append(img_resized.flatten())
+                    img_resized = cv2.resize(img, img_size) # Uniformització imatges
+                    data.append((img_resized / 255.0).flatten()) # Normalització de les imatges i convertir-les a vector unidimensional
                     labels.append(genre)
     
     if not data or not labels:
@@ -27,7 +31,7 @@ def load_images(base_dir, img_size=(128, 128)):
 
     return np.array(data), np.array(labels)
 
-# Codifica les etiquetes i valida el nombre mínim de mostres per classe.
+# Codifica les etiquetes (necessàries per a que els models aprenguin) i valida el nombre mínim de mostres per classe.
 def encode_labels(labels, min_samples_per_class=2):
     label_encoder = LabelEncoder()
     labels_encoded = label_encoder.fit_transform(labels)
@@ -45,8 +49,8 @@ def split_dataset(data, labels, test_size=0.2):
     return X_train, X_test, y_train, y_test
 
 # Preprocessa imatges i les divideix en conjunts d'entrenament i test.
-def preprocess_images(base_dir, img_size=(128, 128), test_size=0.2, min_samples_per_class=2):
-    data, labels = load_images(base_dir, img_size)
+def preprocess_images(base_dir, img_size=(128, 128), color=False, test_size=0.2, min_samples_per_class=2):
+    data, labels = load_images(base_dir, img_size, color)
     labels_encoded, label_encoder = encode_labels(labels, min_samples_per_class)
     X_train, X_test, y_train, y_test = split_dataset(data, labels_encoded, test_size)
     return X_train, X_test, y_train, y_test, label_encoder
