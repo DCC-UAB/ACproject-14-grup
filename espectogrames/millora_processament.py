f163a18c9dc32b71b5f6cbe6b3c7bd81fc4f1ba4
carlotafp26 <1666263@uab.cat>
2024-12-07 12:21:57 +0100

    actualitzacio txt

--- espectogrames/millora_processament.py
+++
@@ -1,8 +0,0 @@
-"""
-1: dividir espectograma en vectors
-2: normalitzar de forma diferent
-3: extreure caracteristiques de frequencia
-4: augmentar dades?
-5: reduir dimensionalitat
-6: fer escala de grisos logaritmica
-"""
